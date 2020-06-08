81efd94844d5d3e68826ea62225c62a1cbea4aa8
hugo <hugo.bricoult0521@gmail.com>
2020-06-08 09:53:12 +0200

    test

---
+++ game/assets/game/js/game.js
@@ -0,0 +1,75 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    fps: 144,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+let game = new Phaser.Game(config);
+
+let player;
+let cursors;
+function preload ()
+{
+    this.load.spritesheet('frog',
+        './game/assets/game/img/frog.png',
+        { frameWidth: 80, frameHeight: 80 }
+    );
+}
+
+function create ()
+{
+    this.physics.world.setFPS(60);
+    player = this.physics.add.sprite(100, 450, 'frog');
+            player.setBounce(0);
+            player.setCollideWorldBounds(true);  //body box 8/48 et décallé de 12 par rapport a la base
+
+            this.anims.create({
+                key: 'left',
+                frames: this.anims.generateFrameNumbers('frog', { start: 0, end: 15 }),
+                frameRate: 15,
+                repeat: -1
+            });
+            this.anims.create({
+                key: 'right',
+                frames: this.anims.generateFrameNumbers('frog', { start: 16, end: 30 }),
+                frameRate: 15,
+                repeat: -1
+            });
+            cursors = this.input.keyboard.createCursorKeys();
+            socket.on('change',(vel)=>{
+                player.setVelocityX(parseInt(vel));
+                player.anims.play('left', true);
+            });
+            socket.on('changer',(vel)=>{
+                player.setVelocityX(parseInt(vel));
+                player.anims.play('right', true);
+            });
+            
+}
+
+function update ()
+{
+    if (cursors.left.isDown ) {
+        player.setVelocityX(player.body.velocity.x -5);
+        socket.emit('turn',player.body.velocity.x -5);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown ) {
+        player.setVelocityX(player.body.velocity.x +5);
+        player.anims.play('right', true);
+        socket.emit('turnr',player.body.velocity.x +5);
+    }
+}
+
---
+++ server.js
@@ -0,0 +1,34 @@
+//Appel des modules de node
+const express = require('express');
+const app = express();
+
+const serv = require('http').Server(app);
+const port = 3000;
+
+let player_list = {};
+let player = {};
+
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '/game/index.html');
+});
+
+app.use('/game',express.static(__dirname + '/game'));
+app.use((req, res, next) => res.status(404).sendFile(__dirname + '/game/404.html'))
+
+serv.listen(process.env.PORT || port);
+console.log(`*** Démarage du serveur sur le port ${port} ***`);
+
+
+//socket io
+var io = require('socket.io').listen(serv);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection',(socket) => {
+    socket.on('turn',(message)=>{
+        socket.broadcast.emit('change',message);
+    });
+    socket.on('turnr',(message)=>{
+        socket.broadcast.emit('changer',message);
+    });
+});
+
